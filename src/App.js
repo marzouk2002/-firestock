c9f34da97cad06f306efe3365d222fec1c22a1d1
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-25 20:36:58 +0100

    Router in home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
