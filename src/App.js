20bb2dbfd98e0edf94f4b5eadf6827ff0fe81945
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-29 15:52:08 +0100

    create the login and register components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
+import Login from './components/Login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
         </Switch>
       </Router>
     </>
