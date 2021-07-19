104c6bf454a5c93f58772f251b1a793c584a5f12
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-07-19 15:23:22 +0100

    setPremiumAccount done

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addPremiumAccount = functions.https.onCall((data, context) => {
+    // get user and add premium custom claim
+    return admin.auth().getUserByEmail(data.email).then(user => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        premium: true
+      })
+    }).then(() => {
+      return {
+        message: `Success! ${data.email} account has been made an premium.`
+      }
+    }).catch(err => {
+      return err;
+    });
+});
