782b3d74dcfeb2de08f6440f9b3deaf39779c0b8
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-09 14:33:32 +0100

    building handling payment

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
+const stripe = require('stripe')(process.env.PV_STR_KEY)
 admin.initializeApp();
 
+// Add a Premium Claim to Users
 exports.setPremiumAccount = functions.https.onCall((data, context) => {
     // get user and add premium custom claim
     return admin.auth().getUserByEmail(data.email).then(user => {
@@ -17,6 +20,20 @@ exports.setPremiumAccount = functions.https.onCall((data, context) => {
     });
 });
 
+// handle Payment via stripe
 exports.handlePayment = functions.https.onCall(({ id, email }) => {
-  return id
+  const amount= 1000
+  stripe.customers.create({
+    email: email,
+    source: id
+  })
+  .then(customer => {
+      stripe.charges.create({
+          amount,
+          description: 'Purchase a EBook',
+          currency: 'usd',
+          customer: customer.id
+      })
+  })
+  .then(charge => res.render('success'))
 })
