f705f2e6354bb1ecf1db164c59c911cb52df7a7d
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-12 22:38:14 +0100

    open and close user info card done

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Logo from '../../images/logo.svg'
 import User from '../../images/user.svg'
 
 function Header({ userInfo }) {
     const [ viewUser, setViewUser ] = useState(false)
     const { picture } = userInfo
+    const userInfoCard = useRef()
+
+    useEffect(() => {
+        if(viewUser) userInfoCard.current.focus()
+    }, [ viewUser ])
+
+    const viewCard = (e) => {
+        e.stopPropagation()
+        setViewUser(true)
+    }
 
     return (
         <header className="d-header">
             <img src={Logo} alt="Logo" className="logo"/>
             <div className="account-panel">
-                <div className="user-logo" tabIndex="0" onClick={()=>setViewUser(!viewUser)} onBlur={()=>setViewUser(false)}>
+                <div className="user-logo" tabIndex="0" onClick={viewCard} >
                     <img src={ picture ? picture : User } alt="user icon"/>
                 </div>
                 { 
-                    viewUser && <div className="user-info">
+                    viewUser && <div className="user-info" tabIndex="0" onBlur={()=>setViewUser(false)} ref={userInfoCard}>
 
                     </div>
                 }
