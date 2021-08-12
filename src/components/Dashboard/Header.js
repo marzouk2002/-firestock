59a2c5eef0916163cdcaf6355bc0bdb098347487
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-12 15:13:21 +0100

    set user logo

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Logo from '../../images/logo.svg'
+import User from '../../images/user.svg'
 
 function Header() {
     return (
         <header>
-            
+            <img src={Logo} alt="Logo" className="logo"/>
+            <div className="account-panel">
+                <img className="user-logo" src={User} alt="user icon"/>
+            </div>
         </header>
     )
 }
