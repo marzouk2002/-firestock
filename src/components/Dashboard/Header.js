6f95ff92f06ce4f54b90353023a09a893d018e0c
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-12 20:57:46 +0100

    handle hide and show user info

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../images/logo.svg'
 import User from '../../images/user.svg'
 
 function Header({ userInfo }) {
+    const [ viewUser, setViewUser ] = useState(false)
     const { picture } = userInfo
 
     return (
         <header className="d-header">
             <img src={Logo} alt="Logo" className="logo"/>
-            <div className="account-panel">
-                <div className="user-logo" tabIndex="0">
+            <div className="account-panel" onBlur={()=>setViewUser(false)}>
+                <div className="user-logo" tabIndex="0" onClick={()=>setViewUser(true)}>
                     <img src={ picture ? picture : User } alt="user icon"/>
                 </div>
-                <div className="user-info">
-                    
-                </div>
+                { 
+                    viewUser && <div className="user-info" onClick={()=>setViewUser(true)}>
+
+                    </div>
+                }
             </div>
         </header>
     )
