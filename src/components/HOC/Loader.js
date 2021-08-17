187f88983a523aa0c29a7fecc25223040d136a5a
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-17 16:14:16 +0100

    loader scss added

--- src/components/HOC/Loader.js
+++ src/components/HOC/Loader.js
@@ -4,14 +4,19 @@ function Loader(WrappedComponent) {
     return class wrapperClass extends React.Component {
         constructor(props) {
             super(props)
-            this.state = {loading: true}
+            this.state = {loading: false}
+            this.setLoading = this.setLoading.bind(this)
+        }
+
+        setLoading(status) {
+            this.setState({ loading: status })
         }
 
         render() {
             return (
                 <>
-                    {this.state.loading && <div className="loader">jjjj</div>} 
-                    <WrappedComponent/>
+                    {this.state.loading && <div className="loader"></div>} 
+                    <WrappedComponent setLoading={this.setLoading}/>
                 </>
             )
         }
